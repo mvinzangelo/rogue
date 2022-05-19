82f717f6358aa60cca5467adf46a50ad36427e2e
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-05-18 20:57:40 -0700

    single file solution

--- vmadr010_custom_lab.ino
+++ vmadr010_custom_lab.ino
@@ -1,8 +1,139 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <LiquidCrystal.h>
 
-// #include <joystick_input.ino>
-// #include <nokia_screen_driver.ino>
-// #include <lcd_screen_driver.ino>
+const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd = LiquidCrystal(rs, en, d4, d5, d6, d7);
+
+const int clk = 9, din = 13, d_c = 12, ce = 10, rst = 11;
+Adafruit_PCD8544 nokiaScreen = Adafruit_PCD8544(clk, din, d_c, ce, rst);
+
+const int xAxis = A0;
+const int yAxis = A1;
+const int joystickBtn = 2;
+const int xAxis_median = 500;
+const int yAxis_median = 500;
+
+enum CODE_INPUT
+{
+  LEFT,
+  UP,
+  DOWN,
+  RIGHT,
+  NEUTRAL
+} input;
+
+static CODE_INPUT currInput = NEUTRAL;
+
+enum SM_JOYSTICK_INPUT_States
+{
+  SM_JOYSTICK_INPUT_INIT,
+  SM_JOYSTICK_INPUT_NEUTRAL,
+  SM_JOYSTICK_INPUT_LEFT,
+  SM_JOYSTICK_INPUT_RIGHT,
+  SM_JOYSTICK_INPUT_UP,
+  SM_JOYSTICK_INPUT_DOWN,
+};
+int SM_JOYSTICK_INPUT_Tick(int state)
+{
+  switch (state)
+  {
+  case SM_JOYSTICK_INPUT_INIT:
+    state = SM_JOYSTICK_INPUT_NEUTRAL;
+    break;
+  case SM_JOYSTICK_INPUT_NEUTRAL:
+    if (analogRead(xAxis) < xAxis_median - 400)
+    {
+      state = SM_JOYSTICK_INPUT_LEFT;
+    }
+    else if (analogRead(xAxis) > xAxis_median + 400)
+    {
+      state = SM_JOYSTICK_INPUT_RIGHT;
+    }
+    else if (analogRead(yAxis) > yAxis_median + 400)
+    {
+      state = SM_JOYSTICK_INPUT_UP;
+    }
+    else if (analogRead(yAxis) < yAxis_median - 400)
+    {
+      state = SM_JOYSTICK_INPUT_DOWN;
+    }
+    else
+    {
+      state = SM_JOYSTICK_INPUT_NEUTRAL;
+    }
+    break;
+  case SM_JOYSTICK_INPUT_LEFT:
+    if (analogRead(xAxis) > xAxis_median - 400)
+    {
+      state = SM_JOYSTICK_INPUT_NEUTRAL;
+    }
+    else
+    {
+      state = SM_JOYSTICK_INPUT_LEFT;
+    }
+    break;
+  case SM_JOYSTICK_INPUT_RIGHT:
+    if (analogRead(xAxis) < xAxis_median + 400)
+    {
+      state = SM_JOYSTICK_INPUT_NEUTRAL;
+    }
+    else
+    {
+      state = SM_JOYSTICK_INPUT_RIGHT;
+    }
+    break;
+  case SM_JOYSTICK_INPUT_UP:
+    if (analogRead(yAxis) < yAxis_median + 400)
+    {
+      state = SM_JOYSTICK_INPUT_NEUTRAL;
+    }
+    else
+    {
+      state = SM_JOYSTICK_INPUT_UP;
+    }
+    break;
+  case SM_JOYSTICK_INPUT_DOWN:
+    if (analogRead(yAxis) > yAxis_median - 400)
+    {
+      state = SM_JOYSTICK_INPUT_NEUTRAL;
+    }
+    else
+    {
+      state = SM_JOYSTICK_INPUT_DOWN;
+    }
+    break;
+  }
+  switch (state)
+  {
+  case SM_JOYSTICK_INPUT_INIT:
+    break;
+  case SM_JOYSTICK_INPUT_NEUTRAL:
+    currInput = NEUTRAL;
+    // Serial.println("NEUTRAL");
+    break;
+  case SM_JOYSTICK_INPUT_LEFT:
+    currInput = LEFT;
+    // Serial.println("LEFT");
+    break;
+  case SM_JOYSTICK_INPUT_RIGHT:
+    currInput = RIGHT;
+    // Serial.println("RIGHT");
+    break;
+  case SM_JOYSTICK_INPUT_UP:
+    currInput = UP;
+    // Serial.println("UP");
+    break;
+  case SM_JOYSTICK_INPUT_DOWN:
+    currInput = DOWN;
+    // Serial.println("DOWN");
+    break;
+  }
+
+  return state;
+}
 
 typedef struct task
 {
@@ -13,7 +144,7 @@ typedef struct task
 } task;
 
 int delay_gcd;
-const unsigned short tasksNum = 2;
+const unsigned short tasksNum = 1;
 task tasks[tasksNum];
 
 void setup()
@@ -24,11 +155,6 @@ void setup()
   // randomSeed(analogRead(0));
 
   unsigned char i = 0;
-  tasks[i].state = SM_LCD_DRIVER_INIT;
-  tasks[i].period = 200;
-  tasks[i].elapsedTime = 0;
-  tasks[i].TickFct = &SM_LCD_DRIVER_Tick;
-  i++;
   tasks[i].state = SM_JOYSTICK_INPUT_INIT;
   tasks[i].period = 100;
   tasks[i].elapsedTime = 0;
@@ -37,6 +163,7 @@ void setup()
   delay_gcd = 100; // GCD
   lcd.begin(16, 2);
   pinMode(joystickBtn, INPUT_PULLUP);
+
   nokiaScreen.setRotation(2);
   nokiaScreen.begin();
   nokiaScreen.clearDisplay();
@@ -46,7 +173,7 @@ void setup()
   nokiaScreen.setCursor(0, 0);
   nokiaScreen.println("Hello world!");
   nokiaScreen.display();
-  lcd.begin(16, 2);
+
   lcd.clear();
   lcd.print("Hello, world!");
 }
